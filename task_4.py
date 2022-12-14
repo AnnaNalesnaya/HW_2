482b495b3bdead574e813a16ffb0ef257bf04230
Anna Nalesnaya <nalesnaya.anna@mail.ru>
2022-12-14 21:37:09 +0300

    Решение первой задачи. Знаю, что можно проще, но я поняла решение

---
+++ task_4.py
@@ -0,0 +1,20 @@
+"""
+Задайте список из N элементов, заполненный целыми числами из промежутка [-N, N].
+Найдите произведение элементов на индексах, хранящихся в файле indexes.txt (в одной строке один индекс).
+Решение должно работать при любом натуральном N.
+
+Ввод: значение типа <int>
+Вывод: значение типа <int>
+"""
+from random import randint
+number = int(input("введите число N "))
+my_list = [randint(-number, number) for _ in range(number)]
+print(my_list)
+
+res = 1
+with open('indexes.txt', 'r') as file:
+    for line in file:
+        index = int(line)
+        if number > index >= -number:
+            res *= my_list[index]
+print(res)
